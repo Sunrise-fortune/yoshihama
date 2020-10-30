ca20de28a0aeeaf34f29d78ff2ed34a9fd7f38a8
Yoshihama <you@example.com>
2020-10-30 20:17:07 +0900

    first commit

---
+++ var/cache/prod/twig/80/80eba05ba6ab25e58c82423161daec0c5b6d211c12d0faa2776f051f0234cc27.php
@@ -0,0 +1,66 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* @common/lang.twig */
+class __TwigTemplate_d5ae974f070565184823fe512c0cbdba311caef572f73b7d1b15d20cef031d44 extends \Eccube\Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "<script>
+var eccube_lang = {
+    \"common.delete_confirm\":\"";
+        // line 3
+        echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\TranslationExtension']->trans("common.delete_confirm"), "html", null, true);
+        echo "\"
+}
+</script>";
+    }
+
+    public function getTemplateName()
+    {
+        return "@common/lang.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  41 => 3,  37 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "@common/lang.twig", "/usr/home/haw1014ztbfb/html/eccube/src/Eccube/Resource/template/common/lang.twig");
+    }
+}
---
+++ var/cache/prod/twig/f7/f759d90dcd59d07c4a0777649ca32c013e6c97ebc037349f09c02d392b765d75.php
@@ -0,0 +1,63 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* @admin/Form/bootstrap_4_layout.html.twig */
+class __TwigTemplate_d02ec645dc4bbc2eb1a1bb02e9e474be6b3e2c5d5ac91b1ef560a7eb14021f7c extends \Eccube\Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doGetParent(array $context)
+    {
+        // line 11
+        return "bootstrap_4_layout.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $this->parent = $this->loadTemplate("bootstrap_4_layout.html.twig", "@admin/Form/bootstrap_4_layout.html.twig", 11);
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    public function getTemplateName()
+    {
+        return "@admin/Form/bootstrap_4_layout.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  34 => 11,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "@admin/Form/bootstrap_4_layout.html.twig", "/usr/home/haw1014ztbfb/html/eccube/src/Eccube/Resource/template/admin/Form/bootstrap_4_layout.html.twig");
+    }
+}
